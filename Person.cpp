0e91515086727b40b08baceadc1b38350ad3a56a
KingdomCross <alexchheng1998@yahoo.com>
2019-12-03 15:05:10 -0800

    Initial commit

---
+++ Person.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexc on 12/3/2019.
+//
+
+#include "Person.h"
---
+++ Person.h
@@ -0,0 +1,14 @@
+//
+// Created by alexc on 12/3/2019.
+//
+
+#ifndef LAB9_PERSON_H
+#define LAB9_PERSON_H
+
+
+class Person {
+
+};
+
+
+#endif //LAB9_PERSON_H
